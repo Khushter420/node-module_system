9e5ac5b2274bbf71b770be21f223adcf1656763f
Khushter Nizame <66989627+Khushter420@users.noreply.github.com>
2021-05-30 17:43:34 +0530

    Update app.js
    express,router

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const http=require('http');
-const fs=require('fs');
+
 const express=require('express');
-const router=require('./prac/route');
+const bodyParser=require('body-parser');
+const app=express();
+const adminRoute=require('./routes/admin');
+const shopRoute=require('./routes/shop');
 
-const server=http.createServer(handler);
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(adminRoute);
+app.use(shopRoute);
 
-server.listen(8000);
+app.use((req,res,next)=>
+{
+	res.status(404).send('<h1>Page Not Found</h1>');
+})
+
+app.listen(8000);
